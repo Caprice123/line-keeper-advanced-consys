2899d6ec97f5068934df58cd79de11f312d27182
Kelvin Cendra <kaguya260401@gmail.com>
2021-10-16 22:32:24 +0700

    image test

--- server/app/routes.py
+++ server/app/routes.py
@@ -12,9 +12,15 @@ counter = Value('i', 0)
 
 def save_img(img):
 	img_dir = "esp32_imgs"
+ 
+ 
+	with counter.get_lock():
+	    counter.value += 1
+	    count = counter.value
+ 
 	if not os.path.isdir(img_dir):
 		os.mkdir(img_dir)
-	cv2.imwrite(os.path.join(img_dir,"img_reg.jpg"), img) # esp32_imgs/img_reg.jpg
+	cv2.imwrite(os.path.join(img_dir,f"img_reg_{count}.jpg"), img) # esp32_imgs/img_reg.jpg
 	# print("Image Saved", end="\n") # debug
 
 @app.route('/')
@@ -29,12 +35,12 @@ def upload():
 	img = None
 	if received.files:
 		print(received.files['file'])
-		# # convert string of image data to uint8
-		# file  = received.files['file']
-		# nparr = np.fromstring(file.read(), np.uint8)
-		# # decode image
-		# img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-		# # save_img(img)
+		# convert string of image data to uint8
+		file  = received.files['file']
+		nparr = np.fromstring(file.read(), np.uint8)
+		# decode image
+		img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+		save_img(img)
 
 		return "[SUCCESS] Image Received", 201
 	else:
--- server/run.py
+++ server/run.py
@@ -2,5 +2,5 @@ from app import app
 
 
 if __name__ == "__main__":
-    app.run( host="192.168.1.11", port=80, threaded=True, debug=True)
+    app.run( host="192.168.100.102", port=80, threaded=True, debug=True)
     
